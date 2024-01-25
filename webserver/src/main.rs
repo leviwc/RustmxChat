725be5dfb6ca3a03e702e4ad84cd7703890092eb
leviwc <leviwc3@hotmail.com>
2024-01-25 00:44:26 -0300

    feat(Webserver) create a todo api testing libs

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,8 +1,26 @@
+use askama::Template;
+use axum::{
+    debug_handler,
+    extract::State,
+    http::{header::CONTENT_TYPE, Method, StatusCode},
+    response::{Html, IntoResponse, Response},
+    routing::{get, post},
+    Form, Router,
+};
+use serde::Deserialize;
+use std::{
+    env,
+    sync::{Arc, Mutex},
+};
+use tower_http::{
+    cors::{Any, CorsLayer},
+    services::ServeDir,
+};
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
- 
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -10,6 +28,122 @@ async fn main() {
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
- 
-    info!("hello, web server!");
+
+    info!("Starting axum");
+    dotenv::dotenv().ok();
+
+    let _db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+
+    let state = AppState {
+        messages: Mutex::new(vec![]),
+    };
+
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any)
+        .allow_headers([CONTENT_TYPE]);
+
+    let assets_path = std::env::current_dir().unwrap();
+
+    let api_router = Router::new()
+        .route("/hello", get(hello_from_the_server))
+        .route("/todos", post(add_todo))
+        .with_state(Arc::new(state));
+
+    let app = Router::new()
+        .nest("/api", api_router)
+        .route("/", get(hello))
+        .route("/another-page", get(another_page))
+        .nest_service(
+            "/assets",
+            ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+        )
+        .layer(cors);
+
+    let server_url = format!("{host}:{port}");
+
+    let listener = tokio::net::TcpListener::bind(&server_url).await.unwrap();
+    axum::serve(listener, app).await?;
+
+    info!("Server stopped");
+
+    Ok(())
+}
+
+async fn hello_from_the_server() -> &'static str {
+    "Hello!"
+}
+
+#[derive(Template)]
+#[template(path = "todo-list.html")]
+struct TodoList {
+    todos: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct TodoRequest {
+    todo: String,
+}
+
+#[debug_handler]
+async fn add_todo(
+    State(state): State<Arc<AppState>>,
+    Form(todo): Form<TodoRequest>,
+) -> impl IntoResponse {
+    let mut lock = state.messages.lock().unwrap();
+    lock.push(todo.todo);
+
+    let template = TodoList {
+        todos: lock.clone(),
+    };
+
+    HtmlTemplate(template)
+}
+
+pub struct AppState {
+    messages: Mutex<Vec<String>>,
+}
+
+async fn another_page() -> impl IntoResponse {
+    let template = AnotherPageTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "another-page.html")]
+struct AnotherPageTemplate;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+pub struct HelloTemplate;
+
+async fn hello() -> impl IntoResponse {
+    let hello = HelloTemplate {};
+
+    HtmlTemplate(hello)
+}
+
+/// A wrapper type that we'll use to encapsulate HTML parsed by askama into valid HTML for axum to serve.
+struct HtmlTemplate<T>(T);
+
+/// Allows us to convert Askama HTML templates into valid HTML for axum to serve in the response.
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template, serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
